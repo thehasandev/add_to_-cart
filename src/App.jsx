e4808ff34b8de45def71c9e971fe998e91c34b07
Hasan <thehasandev@gmail.com>
2023-12-16 21:24:13 +0600

    add-product-name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import one from "./assets/a.jpg"
 
 function App() {
   return (
-    <div>
-      <h1>My name is Hasan</h1>
+    <div className='max-w-[1400px] mx-auto '>
+     <h1 className='font-bold text-[#111111] text-center pt-4 text-4xl'>Our Best Product</h1>
+     <div className='flex justify-between flex-wrap'>
+        <div className='w-[330px]  '> 
+          <img src={one} alt="" className='w-full'/>
+           <div className='flex justify-between'>
+            <h1 className='font-semibold  text-xl  text-[#111111]'>New Arrival 1</h1>
+            <p className='font-semibold  text-base  text-[#111111]'>44$</p>
+           </div>
+      
+        </div>
+     </div>
     </div>
   )
 }
